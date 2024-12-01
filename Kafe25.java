7ef2c460af070a3fa08a83a373402d4de19bc7e1
Wahyudi-Satriawan-1B-TI <wahyudisatriawanh@gmail.com>
2024-12-01 21:37:52 +0700

    Pertanyaan 1 poin 2

--- Kafe25.java
+++ Kafe25.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Kafe25 {
-    public static void Menu() {
+
+    static String namaPelanggan;
+    static boolean isMember;
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan Nama pelanggan: ");
+        namaPelanggan = sc.nextLine();
+        System.out.print("Apakah terdaftar sebagai member? (true/false): ");
+        isMember = sc.nextBoolean();
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -9,9 +26,6 @@ public class Kafe25 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
-    }
-
-    public static void main(String[] args) {
-        Menu();
+        sc.close();
     }
 }
